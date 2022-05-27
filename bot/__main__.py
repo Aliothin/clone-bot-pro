80fdea8464f71cf28839f340c13c5fc22df76bac
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-05-27 16:56:41 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -48,6 +48,7 @@ from .functions import (
     cancel_mirror,
     mirror_status,
     mirror,
+    bypass,
     clone,
     watch,
     shell,
@@ -167,6 +168,8 @@ help_string_telegraph = f"""<br>
 <br><br>
 <b>/{BotCommands.CloneCommand}</b> [drive_url][gdtot_url]: Copy file/folder to Google Drive
 <br><br>
+<b>/{BotCommands.BypassCommand}</b> [shorten_url]: Bypass Adf.ly, GPLinks, Rocklinks, Droplink Links
+<br><br>
 <b>/{BotCommands.CountCommand}</b> [drive_url][gdtot_url]: Count file/folder of Google Drive
 <br><br>
 <b>/{BotCommands.DeleteCommand}</b> [drive_url]: Delete file/folder from Google Drive (Only Owner & Sudo)
@@ -238,6 +241,7 @@ botcmds = [
     (f"{BotCommands.WatchCommand}", "Mirror yt-dlp supported link"),
     (f"{BotCommands.ZipWatchCommand}", "Mirror yt-dlp supported link as zip"),
     (f"{BotCommands.CloneCommand}", "Copy file/folder to Drive"),
+    (f"{BotCommands.BypassCommand}", "Bypass Shorten Links"),
     (f"{BotCommands.LeechCommand}", "Leech"),
     (f"{BotCommands.ZipLeechCommand}", "Leech and upload as zip"),
     (f"{BotCommands.UnzipLeechCommand}", "Leech and extract files"),
